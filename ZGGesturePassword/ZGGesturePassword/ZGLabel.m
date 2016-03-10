f93162215dd23b076d2ce9c094f3ca3697663a69
Odom Sheng's Mac <Odom_Sheng@163.com>
2016-03-10 17:56:47 +0800

    Fixxed some layout issues

--- ZGGesturePassword/ZGGesturePassword/ZGLabel.m
+++ ZGGesturePassword/ZGGesturePassword/ZGLabel.m
@@ -10,14 +10,6 @@
 
 @implementation ZGLabel
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
-}
-*/
-
 - (void)showNormalMsg:(NSString *)msg
 {
     self.textColor = ZGCircleAndLineNormalColor;
@@ -32,41 +24,6 @@
     self.text = msg;
     self.textAlignment = NSTextAlignmentCenter;
     self.font = [UIFont systemFontOfSize:16.0f];
-//    [UIView animateWithDuration:1.0f
-//                          delay:0
-//         usingSpringWithDamping:0.1f
-//          initialSpringVelocity:1.0f
-//                        options:UIViewAnimationOptionCurveLinear
-//                     animations:^{
-////                         CGAffineTransform transform = CGAffineTransformMakeTranslation(-10, 0);
-////                         self.transform = transform;
-//                         CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"tranform.translation.x"];
-//                         animation.values = @[@(-20),@(0),@(20),@(0),@(-20),@(0),@(20),@(0)];
-//                         animation.duration = 0.2f;
-//                         animation.repeatCount = 2;
-//                         [self.layer addAnimation:animation forKey:@"move"];
-//                     } completion:^(BOOL finished) {
-////                         self.transform = CGAffineTransformIdentity;
-//                         [self.layer removeAllAnimations];
-//                     }];
-//    [UIView animateKeyframesWithDuration:0.5
-//                                   delay:0
-//                                 options:UIViewKeyframeAnimationOptionRepeat
-//                              animations:^{
-//                                  CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"tranform.translation.x"];
-//                                  animation.values = @[@(-20),@(0),@(20),@(0),@(-20),@(0),@(20),@(0)];
-//                                  animation.duration = 0.2f;
-//                                  animation.repeatCount = 2;
-//                                  [self.layer addAnimation:animation forKey:@"move"];
-//                              } completion:^(BOOL finished) {
-//                                  
-//                              }];
-    
-//    CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"transform.tra"];
-//    animation.values = @[@(-20),@(0),@(20),@(0),@(-20),@(0),@(20),@(0)];
-//    animation.duration = 0.2f;
-//    animation.repeatCount = 2;
-//    [self.layer addAnimation:animation forKey:@"move"];
     
     CGPoint position = self.layer.position;
     CGPoint x = CGPointMake(position.x - 16, position.y);
